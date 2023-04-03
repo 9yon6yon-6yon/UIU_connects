b0262ca2d5dac3316eda79a455ad7c89a5a0894d
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2023-04-03 16:29:14 +0600

    logout & offline

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,14 +3,22 @@
 @push('title')
     <title>Post</title>
 @endpush
+<!-- Button trigger modal -->
+
+@include('nav-bar')
 <div class="container pt-3">
-   Post a post
+    @foreach ($posts as $post)
+        <h2>{{ $post->post_title }}</h2>
+        <p>{{ $post->content }}</p>
+        <img src="{{ $post->files }}" alt="Post Image">
+        <small>{{ $post->created_at->diffForHumans() }}</small>
+    @endforeach
+    <button id="post-content" type="button" class="btn btn-outline-primary">
+        Post
+    </button>
+
+
  
 </div>
-
-
-
-
-
 
 @include('footer')
