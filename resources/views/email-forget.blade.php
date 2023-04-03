c6dbc148871555f3f88c448a0c2decaeee256685
LailafinNahar-Tithy <114908221+LailafinNahar-Tithy@users.noreply.github.com>
2023-04-03 23:48:26 +0600

    reset password

---
+++ resources/views/email-forget.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>Forgot Password</title>
+</head>
+
+<body
+    style=" height: 100% !important; width: 100% !important; background-=color: #e1e5ea; margin: 0; padding: 0;
+        background-color: #F0F2F4;">
+    <table border="0" cellpadding="0" cellspacing="0"
+        style="width: 100=%; height: 100%; background-color:
+        #F0F2F4;">
+        <tbody>
+            <tr>
+                <td style="vertical-align: top; background-color: #F0F2F4;">
+                    <table border="0" cellpadding="0" cellspacing="0" align=="center" style="width: 600px;">
+                        <tbody>
+                            <tr>
+                                <td style="text-align: center; padding-top: 40px; pad=ding-bottom: 20px;">
+                                    <h2>UIU Connects</h2>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="background-color: #FFFFFF;padding-bottom:=40px;">
+                                    <table border="0" cellpadding="0" cellspacing=="0" align="center"
+                                        style="width: 520px;">
+                                        <tbody>
+                                            <tr>
+                                                <td
+                                                    style="padding-top: 40px; font-family=:helvetica,arial;
+                                                    font-size:14px; line-height:20px; color:#333333">
+                                                    Hi,
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td
+                                                    style="padding-top: 12px; font-family=:helvetica,arial;
+                                                    font-size:14px; line-height:20px; color:#333333">
+                                                    {!! $body !!}
+                                                </td>
+                                            </tr>
+
+                                            <td align="center" style="padding-top:2=0px;">
+                                                <table border="0" cellpadding="0" c=ellspacing="0" align="center">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td
+                                                                style="padding-left:30px;=padding-top: 10px;
+                                                                    padding-bottom: 10px; background-color: #2D8CFF;
+                                                                    border-=top-left-radius: 4px;
+                                                                    border-bottom-left-radius: 4px;">
+                                                                &nbsp;
+                                                            </td>
+                                                            <td
+                                                                style="background-color:#=2D8CFF;
+                                                                    font-family:helvetica,arial; font-size:16px;
+                                                                    line-height:24px; font=-weight: 700; color:#FFFFFF;
+                                                                    padding-top: 10px; padding-bottom: 10px;">
+                                                                <a style="color:#FFFF=FF; text-decoration: none;"
+                                                                    href="{{ $link }}">Click here to change your password</a>
+                                                            </td>
+                                                            <td
+                                                                style="padding-right:30px=;padding-top: 10px;
+                                                                    padding-bottom: 10px; background-color: #2D8CFF;
+                                                                    border=-top-right-radius: 4px;
+                                                                    border-bottom-right-radius: 4px;">
+                                                                &nbsp;
+                                                            </td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                            <tr>
+                                                <td
+                                                    style="font-family:helvetica,arial; f=ont-size:12px;
+                                                    line-height:18px; color:#999999; text-align:center; padding-=top:
+                                                    10px;">
+                                                    Copyright &copy;2022 UIU Connects. All rights reserved.
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
--- resources/views/reset-password.blade.php
+++ resources/views/reset-password.blade.php
@@ -3,12 +3,68 @@
 @push('title')
     <title>Reset Password</title>
 @endpush
-<div class="container pt-3">
- Password reset form
- 
-</div>
 
+<form action="{{ route('reset.password') }}" method="post">
+    @csrf
+    <section class="vh-100 gradient-custom">
+        <input type="hidden" name="token" value="{{ $token }}">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
 
+                            <div class="mb-md-5 mt-md-4 pb-5">
+
+                                <h2 class="fw-bold mb-2 text-uppercase">Reset Password</h2>
+                                <p class="text-white-50 mb-5">Please enter your email and password!</p>
+
+                                <div class="form-outline form-white mb-4">
+                                    <input type="email" name="email" id="typeEmailX"
+                                        class="form-control form-control-lg" placeholder="Email address"
+                                        value="{{ $email ?? old('email') }}" />
+                                    @if ($errors->has('email'))
+                                        <span class="text-danger"> {{ $errors->first('email') }} </span>
+                                    @endif
+
+                                </div>
+
+                                <div class="form-outline form-white mb-4">
+                                    <input type="password" name="password" id="typePasswordX"
+                                        class="form-control form-control-lg" placeholder="Password" />
+                                    @if ($errors->has('password'))
+                                        <span class="text-danger"> {{ $errors->first('password') }} </span>
+                                    @endif
+
+                                </div>
+                                <div class="form-outline form-white mb-4">
+                                    <input type="password" name="password_confirmation" id="typePasswordX"
+                                        class="form-control form-control-lg" placeholder="Confirm Password" />
+                                    @if ($errors->has('password_confirmation'))
+                                        <span class="text-danger"> {{ $errors->first('password_confirmation') }} </span>
+                                    @endif
+
+                                </div>
+
+                                <button class="btn btn-outline-light btn-lg px-5" type="submit"
+                                    name="rp">Reset Password</button>
+
+                            </div>
+
+                            <div>
+                                <p class="mb-0">Don't have an account? <a href="{{ route('user-sign-up') }}"
+                                        class="text-white-50 fw-bold">Sign Up</a>
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</form>
 
 
 
