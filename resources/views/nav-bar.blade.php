4e2a7e5268aba5700616160dd4ad9a71faf586d2
LailafinNahar-Tithy <114908221+LailafinNahar-Tithy@users.noreply.github.com>
2023-05-01 22:31:32 +0600

    post form

--- resources/views/nav-bar.blade.php
+++ resources/views/nav-bar.blade.php
@@ -1,17 +1,17 @@
 <section class="homepage d-flex">
     <div class="sidemenu bg-light d-flex justify-content-center">
         <div class="sidemenu-icon-container">
-            <a href="{{ route('user.dashboard') }}"> <img src="{{ asset('image/icons/home.png') }}" class="icon-home"></a>
-            <a href="{{ route('user.search') }}"><img src="{{ asset('image/icons/search.png') }}"
+            <a href="{{ route('user.dashboard') }}" class="icon-link"> <img src="{{ asset('image/icons/home.png') }}" class="icon-home"></a>
+            <a href="{{ route('user.search') }}" class="icon-link"><img src="{{ asset('image/icons/search.png') }}"
                     class="icon-contact-info"></a>
-            <a href="{{ route('user.posts') }}"><img src="{{ asset('image/icons/news-feed.png') }}"
+            <a href="{{ route('user.posts') }}" class="icon-link"><img src="{{ asset('image/icons/news-feed.png') }}"
                     class="icon-message"></a>
-            <a href="{{ route('create-post') }}"><img src="{{ asset('image/icons/create.png') }}"
+            <a href="{{ route('create-post') }}" class="icon-link"><img src="{{ asset('image/icons/create.png') }}"
                     class="icon-logout"></a>
-            <a href="{{ route('create-post') }}"><img src="{{ asset('image/icons/chat.png') }}" class="icon-logout"></a>
-            <a href="{{ route('user.settings') }}"><img src="{{ asset('image/icons/settings.png') }}"
+            <a href="{{ route('chat.dashboard') }}" class="icon-link"><img src="{{ asset('image/icons/chat.png') }}" class="icon-logout"></a>
+            <a href="{{ route('user.settings') }}" class="icon-link"><img src="{{ asset('image/icons/settings.png') }}"
                     class="icon-logout"></a>
-            <a href="{{ route('user.profile.all', ['id' => Session::get('$user_id')]) }}"><img
+            <a href="{{ route('user.profile.all', ['id' => Session::get('$user_id')]) }}" class="icon-link"><img
                     src="{{ asset('image/icons/profile.png') }}" class="icon-profile"></a>
 
         </div>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,8 @@
 @include('header')
 @extends('header')
 @push('title')
-    <title>Post</title>
+    <title>
+        Post</title>
 @endpush
 <!-- Button trigger modal -->
 
@@ -9,91 +10,162 @@
 
 <div class="home-right-side ">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="home-user-info sticky-sm-top">
-                    <div class="info-header d-flex flex-column align-items-center ">
-                        <div class="profile-pic-container"></div>
-                        <div class="user-name text-center">
-                            <h1>{{Session::get('$user_email')}}</h1>
-                            <p>Student, UIU </p>
-                        </div>
-                        <div class="user-button">
-                        <a href="{{route('create-post')}}"> <button class="post-btn">Post</button></a> 
-                            <a href="{{route('user.profile.all',['id' => Session::get('$user_id')])}}"><button class="edit-profile-btn">Edit Profile</button></a>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="info-bottom">
-                        <h4 class="about-heading">About</h4>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolore adipisci iusto consectetur quae inventore dolores accusamus facilis ad? Vero!</p>
-                        <h4 class="quick-contact">Quick Contacts</h4> <br>
-                        <img src="{{asset('image/icons/quick-contact-message.png')}}" alt=""> <br>
-                        <img src="{{asset('image/icons/quick-contact-phone.png')}}" alt="">
+        <div class="sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-light  bg-light">
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+                        <ul class="navbar-nav ">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#"
+                                    onclick="showSection('General')">General</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" onclick="showSection('Job')">Jobs</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" onclick="showSection('Event')">Events</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-8">
+            </nav>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
                 <div class="user-post-container">
-                    <div class="user-single-post-with-comment mb-3">
-                        <div class="user-single-post-top">
-                            <div class="sticky-top">
-                                <nav class="navbar navbar-expand-lg navbar-light  bg-light">
-                                    <div class="container-fluid">
-                                      
-                                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                        <span class="navbar-toggler-icon"></span>
-                                      </button>
-                                      <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-                                        <ul class="navbar-nav ">
-                                          <li class="nav-item">
-                                            <a class="nav-link active" aria-current="page" href="#">General</a>
-                                          </li>
-                                          <li class="nav-item">
-                                            <a class="nav-link" href="#">Job</a>
-                                          </li>
-                                          <li class="nav-item">
-                                            <a class="nav-link" href="#">Event</a>
-                                          </li>
-                                        </ul>
-                                      </div>
+
+                    <div id="General-section" class="content-section">
+                        @foreach ($posts as $post)
+                            <div class="user-single-post-with-comment mb-3">
+                                <div class="user-single-post-top">
+                                    <div class="poster-name-with-picture d-flex align-items-center">
+                                        <div class="poster-image">
+                                            <img src="{{ asset('image/PosterAvatar.png') }}" alt="">
+                                        </div>
+                                        <div class="poster-details">
+                                            <h4>{{ $post->post_title }}</h4>
+                                            <small>{{ $post->created_at->diffForHumans() }}</small>
+                                        </div>
                                     </div>
-                                  </nav>
-                            </div>
-                            @foreach ($posts as $post)
-                            <div class="poster-name-with-picture d-flex align-items-center">
-                                <div class="poster-image">
-                                    <img src="{{asset('image/PosterAvatar.png')}}" alt="">
+                                    <div class="poster-post">
+                                        <p>{{ $post->content }}</p>
+
+                                    </div>
+
+                                    <div class="post-img">
+                                        <img src="{{ asset('storage/files/' . $post->files) }}" alt="">
+                                    </div>
+
+                                    <hr>
+                                    <div class="like-comment-area">
+                                        <a href="{{ route('posts.upvote', $post->post_id) }}">
+                                            <button type="submit"><img src="{{ asset('image/like.png') }}"
+                                                    height="20px" alt="upvote"> {{ $post->upvotes }} </button>
+                                        </a>
+                                        <a href="{{ route('posts.downvote', $post->post_id) }}">
+                                            <button type="submit"><img src="{{ asset('image/dislike.png') }}"
+                                                    height="20px" alt="downvote"> -{{ $post->downvotes }}</button>
+                                        </a>
+                                    </div>
+                                    <hr>
                                 </div>
-                                <div class="poster-details">
-                                    <h2>{{ $post->post_title }}</h2>
-                                    <small>{{ $post->created_at->diffForHumans() }}</small>
+                            </div>
+                        @endforeach
+                    </div>
+                    <div id="Job-section" class="content-section" style="display:none">
+                        @foreach ($jobs as $post)
+                            <div class="user-single-post-with-comment mb-3">
+                                <div class="user-single-post-top">
+                                    <div class="poster-name-with-picture d-flex align-items-center">
+                                        <div class="poster-image">
+                                            <img src="{{ asset('image/PosterAvatar.png') }}" alt="">
+                                        </div>
+                                        <div class="poster-details">
+                                            <h4>{{ $post->job_title }}</h4>
+                                            <small>{{ $post->created_at->diffForHumans() }}</small>
+                                        </div>
+                                    </div>
+                                    <div class="poster-post">
+                                        <p>{{ Str::limit($post->job_details, $limit = 200, $end = '...') }}
+                                            @if (strlen($post->job_details) > 200)
+                                                <a href="{{ route('view.p.job', ['id' => $post->job_id]) }}">See
+                                                    more</a>
+                                            @endif
+                                        </p>
+                                    </div>
+                                    <div class="post-img">
+                                        <img src="{{ asset('storage/files/' . $post->files) }}" alt=""
+                                            class="w-100">
+                                    </div>
+                                    <hr>
                                 </div>
                             </div>
-                            <div class="poster-post">
-                                <p>{{$post->content}}</p>
-                            </div>   
-                            <hr>
-                            <div class="like-comment-area">
-                                <form action="{{ route('posts.upvote', $post->post_id) }}" method="POST">
-                                    @csrf
-                                    <button type="submit"><img src="{{ asset('image/like.png') }}" height="20px" alt="upvote"> {{ $post->upvotes }} </button>
-                                </form>
-                                
-                                <form action="{{ route('posts.downvote', $post->post_id) }}" method="POST">
-                                    @csrf
-                                    <button type="submit"><img src="{{ asset('image/dislike.png') }}" height="20px" alt="downvote"> {{ $post->downvotes }}</button>
-                                </form>
-                                <button><img src="{{asset('image/comment.png')}}" height="20px" alt=""> Comment</button>
+                        @endforeach
+                    </div>
+                    <div id="Event-section" class="content-section" style="display:none">
+                        @foreach ($events as $event)
+                            <div class="user-single-post-with-comment mb-3">
+                                <div class="user-single-post-top">
+                                    <div class="poster-details d-flex justify-content-between">
+                                        <div class="poster-name-with-picture d-flex align-items-center">
+                                            <div class="poster-image">
+                                                <img src="{{ asset('image/PosterAvatar.png') }}" alt="">
+                                            </div>
+                                            <div class="poster-details">
+                                                <h4>{{ $event->event_title }}</h4>
+                                                <small>{{ $event->created_at->diffForHumans() }}</small>
+                                            </div>
+                                        </div>
+                                        <div class="event-date">
+                                            <small>Event date: {{ $event->event_date }}</small>
+                                        </div>
+                                    </div>
+                                    <div class="poster-post">
+                                        <p>{{ Str::limit($event->event_details, $limit = 200, $end = '...') }}
+                                            @if (strlen($event->event_details) > 200)
+                                                <a href="{{ route('view.p.event', ['id' => $event->event_id]) }}">See
+                                                    more</a>
+                                            @endif
+                                        </p>
+                                    </div>
+
+                                    <div class="post-img">
+                                        <img src="{{ asset('storage/files/' . $event->files) }}" alt="">
+                                    </div>
+                                    <hr>
+                                </div>
                             </div>
-                            <hr>
-                            @endforeach
-                        </div>
-                    </div> 
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
-    </div> 
+    </div>
 </div>
+
 </section>
+
+<script>
+    const menuItems = document.querySelectorAll('.nav-item');
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            menuItems.forEach(item => item.querySelector('a').classList.remove('active'));
+            item.querySelector('a').classList.add('active');
+        });
+    });
+
+    function showSection(sectionName) {
+
+        var contentSections = document.getElementsByClassName("content-section");
+        for (var i = 0; i < contentSections.length; i++) {
+            contentSections[i].style.display = "none";
+        }
+        var selectedSection = document.getElementById(sectionName + "-section");
+        selectedSection.style.display = "block";
+    }
+</script>
 @include('footer')
